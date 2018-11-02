bcaabe4cf8d29cdae02bcb0ab57293e209e4dc2d
BJ Bloemker <bjbloemker@gmail.com>
2018-11-02 16:27:03 -0500

    Add gson support

--- src/main/java/com/bjbloemker/resources/HelloWorldResource.java
+++ src/main/java/com/bjbloemker/resources/HelloWorldResource.java
@@ -3,13 +3,14 @@ import com.codahale.metrics.annotation.Timed;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 
 
-@Path("/hello")
+@Path("/parkpay/name/{name}")
 public class HelloWorldResource {
     @GET
-    public String helloWorld(@QueryParam("name") String name) {
+    public String helloWorld(@PathParam("name") String name) {
         if(name != null)
             return "Hello " + name + "!";
         else
